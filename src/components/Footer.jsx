a677899ae9f468b8264cf6bfef8b0113f4c9b0a8
unknown <hafizhamza78699@gmail.com>
2024-02-15 21:15:07 +0500

    portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p className="m-0 text-center">© 2023 Hamza Malik copyright all right reserved</p>
+        <p className="m-0 text-center">Developed By Hamza</p>
+      </div>
+    </footer>
+  );
+}
